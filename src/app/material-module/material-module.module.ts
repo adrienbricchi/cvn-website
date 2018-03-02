bfa9d041797cfa28032ba27ab09993d5010112dc
Adrien Bricchi <adrienbricchi@users.noreply.github.com>
2018-03-02 20:59:23 +0100

    Location done

--- src/app/material-module/material-module.module.ts
+++ src/app/material-module/material-module.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material';
+import {MatIconModule} from '@angular/material';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
@@ -7,12 +8,14 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
   imports: [
     BrowserAnimationsModule,
     MatButtonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule
   ],
   exports: [
     BrowserAnimationsModule,
     MatButtonModule,
-    MatGridListModule
+    MatGridListModule,
+    MatIconModule
   ]
 })
 export class MaterialModule {}
